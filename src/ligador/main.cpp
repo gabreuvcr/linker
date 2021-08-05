fddc69c622834c38e8b19ae94a92587231b2a7fc
Caio Guedes <caioguedesam@gmail.com>
2021-08-05 16:54:07 -0300

    main

--- src/ligador/main.cpp
+++ src/ligador/main.cpp
@@ -9,11 +9,23 @@ int main(int argc, char *argv[]) {
     std::vector<std::string> commands;
     std::map<std::string, int> labels = {};
 
-    update_table(argv[1], labels, commands, ILC);
-    update_table(argv[2], labels, commands, ILC);
+    for(int i = 1; i < argc; i++) {
+        update_table(argv[i], labels, commands, ILC);
+    }
     update_code(labels, commands);
 
     int size = commands.size();
+    int program_start = 100;
+    int stack_start = 1000 + program_start + size;
+    int entry_point = program_start + labels["main"] - 1;
+
+    std::cout << "MV-EXE" << std::endl << std::endl;
+
+    std::cout << size << " ";
+    std::cout << program_start << " ";
+    std::cout << stack_start << " ";
+    std::cout << entry_point << std::endl << std::endl;
+
     for (int i = 0; i < size; i++) {
         std::cout << commands[i] << " ";
     }
